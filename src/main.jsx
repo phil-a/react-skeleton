e38d535a24187a0f2af805c50a4616063f992127
Philip Alekseev <phil.alekseev@gmail.com>
2016-03-01 00:07:30 -0500

    first commit - basic setup

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var List = require('./components/List.jsx');
+
+//Grab the DOM, insert the list, and put it in element that has ingredients id
+ReactDOM.render(<List />, document.getElementById('ingredients'));
